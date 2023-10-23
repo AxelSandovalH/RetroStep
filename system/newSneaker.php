5a689c8e076c8103369516028aff1efc768bec41
Francisco <frodriguez25@ucol.mx>
2023-10-22 20:32:21 -0600

    Working with new DB

--- system/newSneaker.php
+++ system/newSneaker.php
@@ -27,12 +27,11 @@ if(!empty($_POST)){
     WHERE (sneaker_name = '$sneaker_name')");
 
     $result = mysqli_fetch_array($querySelect);
-    
+
     if($result > 0){
         echo '<p class ="msgError">Ese modelo ya existe</p>';
     }
     else{
-        $id_stock = rand(1, 100);
 
         $queryInsert = mysqli_multi_query($connection, 
         "INSERT INTO brand (brand_name) VALUES ('$brand_name')
@@ -45,8 +44,11 @@ if(!empty($_POST)){
         ON DUPLICATE KEY UPDATE category_name = '$category_name';
         
         INSERT INTO sneaker (brand_name, sneaker_name, price, size_number, category_name) VALUES ('$brand_name', '$sneaker_name', $price, $size_number, '$category_name');
-        INSERT INTO stock (stock_quantity, id_stock) VALUES ($stock_quantity, $id_stock);
-        INSERT INTO sneaker_stock (sneaker_id, id_stock) VALUE (?, $id_stock)");
+        -- Obtener el 'sneaker_id' recién generado
+        SET @sneaker_id = LAST_INSERT_ID();
+
+        -- Insertar en la tabla 'stock' con 'sneaker_id' y 'stock_quantity'
+        INSERT INTO stock (sneaker_id, stock_quantity, size_number) VALUES (@sneaker_id, $stock_quantity, $size_number);");
        
 
 
@@ -54,10 +56,12 @@ if(!empty($_POST)){
                 header("location: main.php");
             }
             else{
-                echo '<p class = "msgError"> Error al insertar </p>';
+                echo "Error: " . mysqli_error($connection);
+                echo "Filas afectadas: " . mysqli_affected_rows($connection);
+
             }
     }
-    }
+}
 // $query = mysqli_query($connection, 
 
 // "SELECT * from sneaker
@@ -142,35 +146,25 @@ if(!empty($_POST)){
             <label for="Stock">Stock</label>
             <input name="stock_quantity" type="number" required>
             <label for="Size">Size</label>
-            <input type="number" name="size_number" required>
+            <select name="size_number" required>
+                <option value="6.0">6.0</option>
+                <option value="6.5">6.5</option>
+                <option value="7.0">7.0</option>
+                <option value="7.5">7.5</option>
+                <option value="8.0">8.0</option>
+                <option value="8.5">8.5</option>
+                <option value="9.0">9.0</option>
+                <option value="9.5">9.5</option>
+                <option value="10.0">10.0</option>
+                <option value="10.5">10.5</option>
+                <option value="11.0">11.0</option>
+                <option value="11.5">11.5</option>
+                <option value="12.0">12.0</option>
+                <option value="12.5">12.5</option>
+                <option value="13.0">13.0</option>
+            </select>
             <label for="Category">Category</label>
             <input type="text" name="category_name" required>
-
-            <!-- <select name="Size" type="number">
-                <option value="3">3</option>
-                <option value="3.5">3.5</option>
-                <option value="4">4</option>
-                <option value="4.5">4.5</option>
-                <option value="5">5</option>
-                <option value="5.5">5.5</option>
-                <option value="6">6</option>
-                <option value="6.5">6.5</option>
-                <option value="7">7</option>
-                <option value="7.5">7.5</option>
-                <option value="8">8</option>
-                <option value="8.5">8.5</option>
-                <option value="9">9</option>
-                <option value="9.5">9.5</option>
-                <option value="10">10</option>
-                <option value="10.5">10.5</option>
-                <option value="11">11</option>
-                <option value="11.5">11.5</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-            </select>
-            
-     -->
             
             <button type="reset" id="Cancelar"><a href="main.php">Cancelar</a></button>
             
