3ddd7bfd73968f72778f54b710a78e79c9dbe63b
joseluis-dls <jdelossantos1@ucol.mx>
2023-10-26 18:26:41 -0600

    Recuperacion de archivos EDITAR y ELIMINAR usuarios

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
     $host = 'localhost';
-    $user = 'paco';
-    $password = '2754';
+    $user = 'jl';
+    $password = '123';
     $db = 'retro_step';
     $port = '3307';
     $connection = @mysqli_connect($host, $user, $password, $db, $port);
