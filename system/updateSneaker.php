7d3e6334c6ea9831bb57cd08aa309d4269d29fdf
Francisco <frodriguez25@ucol.mx>
2023-10-31 12:23:37 -0600

    Accept button div fixed

--- system/updateSneaker.php
+++ system/updateSneaker.php
@@ -138,18 +138,18 @@ if ($result_sql == 0) {
                 <label for="Marca">Marca</label>
                 <select name="brand_name" required>
                     <?php
-                     $sql_brands = "SELECT brand_name FROM brand";
-                    $result_brands = mysqli_query($connection, $sql_brands);
-                
-                    if (mysqli_num_rows($result_brands) > 0) {
-                        while ($row = mysqli_fetch_assoc($result_brands)) {
-                            $brand_option = $row['brand_name'];
-                            $selected = ($brand_option == $brand_name) ? "selected" : "";
-                            echo "<option value='$brand_option' $selected>$brand_option</option>";
+                        $sql_brands = "SELECT brand_name FROM brand";
+                        $result_brands = mysqli_query($connection, $sql_brands);
+                    
+                        if (mysqli_num_rows($result_brands) > 0) {
+                            while ($row = mysqli_fetch_assoc($result_brands)) {
+                                $brand_option = $row['brand_name'];
+                                $selected = ($brand_option == $brand_name) ? "selected" : "";
+                                echo "<option value='$brand_option' $selected>$brand_option</option>";
+                            }
+                        } else {
+                            echo "<option value=''>No hay marcas disponibles</option>";
                         }
-                    } else {
-                        echo "<option value=''>No hay marcas disponibles</option>";
-                    }
                     ?>
                 </select>
                 <label for="Precio">Precio</label>
@@ -160,29 +160,30 @@ if ($result_sql == 0) {
                 <label for="Stock">Stock</label>
                 <input name="stock_quantity" type="number" value="<?php echo $stock_quantity; ?>" required>
                 <label for="Size">Size</label>
-                <label for="size_number">Talla</label>
                 <select name="size_number" required>
                     <?php
-                    $sql_sizes = "SELECT size_number FROM size";
-                    $result_sizes = mysqli_query($connection, $sql_sizes);
-                
-                        if (mysqli_num_rows($result_sizes) > 0) {
-                        while ($row = mysqli_fetch_assoc($result_sizes)) {
-                            $size_option = $row['size_number'];
-                            $selected = ($size_option == $size_number) ? "selected" : "";
-                            echo "<option value='$size_option' $selected>$size_option</option>";
+                        $sql_sizes = "SELECT size_number FROM size";
+                        $result_sizes = mysqli_query($connection, $sql_sizes);
+                    
+                            if (mysqli_num_rows($result_sizes) > 0) {
+                            while ($row = mysqli_fetch_assoc($result_sizes)) {
+                                $size_option = $row['size_number'];
+                                $selected = ($size_option == $size_number) ? "selected" : "";
+                                echo "<option value='$size_option' $selected>$size_option</option>";
+                            }
+                        } else {
+                            echo "<option value=''>No hay tallas disponibles</option>";
                         }
-                    } else {
-                        echo "<option value=''>No hay tallas disponibles</option>";
-                    }
                     ?>
                 </select>
-            </div>          
-        </form>
-        <div class="buttons">
+            </div> 
+                   
+            <div class="buttons">
                 <button type="reset" id="Cancelar"><a href="main.php">Cancelar</a></button>
                 <button type="submit" id="Succes">Actualizar</button>
-            </div>  
+            </div>    
+        </form>
+        
     </div>
     
     
