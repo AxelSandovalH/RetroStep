9f02e947e4bd5fef7a68a69a1d13c7d674db9cab
joseluis-dls <jdelossantos1@ucol.mx>
2023-11-07 11:25:27 -0600

    Pull tryPacoAxel

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
     $host = 'localhost';
-    $user = 'root';
-    $password = 'psyduck56';
+    $user = 'jl';
+    $password = '123';
     $db = 'retro_step';
     $port = '3306';
     $connection = @mysqli_connect($host, $user, $password, $db, $port);
