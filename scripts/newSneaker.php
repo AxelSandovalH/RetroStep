db7000bcdcecc54f399fe4b727785da7c29b11d4
Francisco <frodriguez25@ucol.mx>
2023-11-09 09:09:36 -0600

    Pull completed

--- scripts/newSneaker.php
+++ scripts/newSneaker.php
@@ -30,7 +30,6 @@ if (!empty($_POST)) {
         echo 'That model already exists.';
     } else {
         $queryInsert = mysqli_multi_query($connection, 
-<<<<<<< HEAD
         "INSERT INTO sneaker (brand_name, sneaker_name, price, size_number, category_name, imagen_url) VALUES ('$brand_name', '$sneaker_name', $price, $size_number, '$category_name', '$ruta');
         -- Obtener el 'sneaker_id' recién generado
         SET @sneaker_id = LAST_INSERT_ID();
@@ -47,21 +46,6 @@ if (!empty($_POST)) {
                 echo "Filas afectadas: " . mysqli_affected_rows($connection);
 
             }
-=======
-            "INSERT INTO brand (brand_name) VALUES ('$brand_name') ON DUPLICATE KEY UPDATE brand_name = '$brand_name';
-            INSERT INTO size (size_number) VALUES ($size_number) ON DUPLICATE KEY UPDATE size_number = $size_number;
-            INSERT INTO category (category_name) VALUES ('$category_name') ON DUPLICATE KEY UPDATE category_name = '$category_name';
-            INSERT INTO sneaker (brand_name, sneaker_name, price, size_number, category_name, imagen_url) VALUES ('$brand_name', '$sneaker_name', $price, $size_number, '$category_name', '$ruta');
-            SET @sneaker_id = LAST_INSERT_ID();
-            INSERT INTO stock (sneaker_id, stock_quantity, size_number) VALUES (@sneaker_id, $stock_quantity, $size_number);");
-
-        if ($queryInsert) {
-            echo 'Sneaker saved successfully';
-        } else {
-            // Manejo de errores más detallado
-            echo "Error: " . mysqli_error($connection);
-        }
->>>>>>> 3305327752e38d88477e4d1070462f7e8cfc4fbe
     }
 }
 ?>
