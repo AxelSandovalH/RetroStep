3305327752e38d88477e4d1070462f7e8cfc4fbe
Axel <axesan917@gmail.com>
2023-11-09 08:02:15 -0600

    Problema arreglado addSneaker

--- scripts/newSneaker.php
+++ scripts/newSneaker.php
@@ -2,81 +2,47 @@
 require_once("../connection.php");
 
 // Espera a que haya una acción tipo POST para realizar la verificación 
-if(!empty($_POST)){
-    // $sneaker_id = $_POST['sneaker_id'];
+if (!empty($_POST)) {
     $sneaker_name = $_POST["sneaker_name"];
     $brand_name = $_POST["brand_name"];
     $price = $_POST["price"];
     $stock_quantity = $_POST["stock_quantity"];
     $size_number = $_POST["size_number"];
     $category_name = $_POST["category_name"];
-    $nombre_imagen = $_FILES['imagen']['name'];
-    $temporal = $_FILES['imagen']['tmp_name'];
+    
+    // Rutas y carpetas
+    $nombre_imagen = $_FILES['image']['name'];
+    $temporal = $_FILES['image']['tmp_name'];
     $carpeta = __DIR__ . "/img";
+
+    // Verifica si la carpeta existe, si no, créala
+    if (!file_exists($carpeta)) {
+        mkdir($carpeta, 0777, true);
+    }
+
     $ruta = $carpeta . '/' . $nombre_imagen;
-    move_uploaded_file($temporal, $carpeta . '/' . $nombre_imagen);
+    move_uploaded_file($temporal, $ruta);
 
     $querySelect = mysqli_query($connection, 
-    "SELECT * FROM sneaker 
-    WHERE (sneaker_name = '$sneaker_name')");
+        "SELECT * FROM sneaker WHERE sneaker_name = '$sneaker_name'");
 
-    $result = mysqli_fetch_array($querySelect);
-
-    if($result > 0){
+    if (mysqli_num_rows($querySelect) > 0) {
         echo 'That model already exists.';
-    }
-    else{
-
+    } else {
         $queryInsert = mysqli_multi_query($connection, 
-        "INSERT INTO brand (brand_name) VALUES ('$brand_name')
-        ON DUPLICATE KEY UPDATE brand_name = '$brand_name';
-        
-        INSERT INTO size (size_number) VALUES ($size_number)
-        ON DUPLICATE KEY UPDATE size_number = $size_number;
+            "INSERT INTO brand (brand_name) VALUES ('$brand_name') ON DUPLICATE KEY UPDATE brand_name = '$brand_name';
+            INSERT INTO size (size_number) VALUES ($size_number) ON DUPLICATE KEY UPDATE size_number = $size_number;
+            INSERT INTO category (category_name) VALUES ('$category_name') ON DUPLICATE KEY UPDATE category_name = '$category_name';
+            INSERT INTO sneaker (brand_name, sneaker_name, price, size_number, category_name, imagen_url) VALUES ('$brand_name', '$sneaker_name', $price, $size_number, '$category_name', '$ruta');
+            SET @sneaker_id = LAST_INSERT_ID();
+            INSERT INTO stock (sneaker_id, stock_quantity, size_number) VALUES (@sneaker_id, $stock_quantity, $size_number);");
 
-        INSERT INTO category (category_name) VALUES ('$category_name')
-        ON DUPLICATE KEY UPDATE category_name = '$category_name';
-        
-        INSERT INTO sneaker (brand_name, sneaker_name, price, size_number, category_name, imagen_url) VALUES ('$brand_name', '$sneaker_name', $price, $size_number, '$category_name', '$ruta');
-        -- Obtener el 'sneaker_id' recién generado
-        SET @sneaker_id = LAST_INSERT_ID();
-
-        -- Insertar en la tabla 'stock' con 'sneaker_id' y 'stock_quantity'
-        INSERT INTO stock (sneaker_id, stock_quantity, size_number) VALUES (@sneaker_id, $stock_quantity, $size_number);");
-       
-
-            
-            if($queryInsert){
-                echo 'Sneaker saved succesfully';
-            }
-            else{
-                echo "Error: " . mysqli_error($connection);
-                echo "Filas afectadas: " . mysqli_affected_rows($connection);
-
-            }
+        if ($queryInsert) {
+            echo 'Sneaker saved successfully';
+        } else {
+            // Manejo de errores más detallado
+            echo "Error: " . mysqli_error($connection);
+        }
     }
 }
-// $query = mysqli_query($connection, 
-
-// "SELECT * from sneaker
-// INNER JOIN stock
-// ON sneaker.sneaker_id = stock.sneaker_id
-// WHERE sneaker.sneaker_id = $sneaker_id AND stock.sneaker_id = $sneaker_id");
-
-// $result = mysqli_num_rows($query);
-
-// if($result > 0){
-//     while($column = mysqli_fetch_array($query)){
-
-//         $sneaker_name = $column["sneaker_name"];
-//         $brand_name = $column["brand_name"];
-//         $price = $column["price"];
-//         $stock_quantity = $column["stock_quantity"];
-//         $size_number = $column["size_number"];
-//     }
-// }
-// else{
-//     header("location: main.php");
-// }
-
 ?>
