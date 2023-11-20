8c2df9aee1a3a78b2d180bbd849ec5fe8f93ff34
Axel <axesan917@gmail.com>
2023-11-20 11:18:45 -0600

    Reparación de filtros en el rol2

--- contents/sneakers-content_rol2.php
+++ contents/sneakers-content_rol2.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <script src="https://kit.fontawesome.com/c7fad14ccd.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
@@ -8,21 +9,81 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sneakers</title>
 </head>
+
 <body>
+
     <div class="tab">
         <label class="tab-label" for="filterTab"><i class="fa-solid fa-chevron-down"></i></label>
         <input type="checkbox" id="filterTab" class="tab-checkbox">
         <div class="filter-container">
-            <!-- Agrega tus etiquetas de filtro aquí -->
+            <label for="brand-filter">Marca:</label>
+            <select id="brand-filter" class="filter-dropdown">
+                <option value="">Todas las marcas</option>
+                <?php
+                require_once "connection.php";
+                $sql = "SELECT DISTINCT brand_name FROM sneaker WHERE sneaker.deleted_at IS NULL";
+                $result = mysqli_query($connection, $sql);
+
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $brandName = $row['brand_name'];
+                    echo "<option value='$brandName'>$brandName</option>";
+                }
+                ?>
+            </select>
+            <label for="size-filter">Talla:</label>
+            <select id="size-filter" class="filter-dropdown">
+                <option value="">Todas las tallas</option>
+                <?php
+                $sql = "SELECT DISTINCT size_number FROM size";
+                $result = mysqli_query($connection, $sql);
+
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $sizeNumber = $row['size_number'];
+                    echo "<option value='$sizeNumber'>$sizeNumber</option>";
+                }
+                ?>
+            </select>
+
+            <label for="model-filter">Modelo:</label>
+            <select id="model-filter" class="filter-dropdown">
+                <option value="">Todos los modelos</option>
+                <?php
+                $sql = "SELECT DISTINCT sneaker_name FROM sneaker WHERE deleted_at IS NULL";
+                $result = mysqli_query($connection, $sql);
+
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $sneakerName = $row['sneaker_name'];
+                    echo "<option value='$sneakerName'>$sneakerName</option>";
+                }
+                ?>
+            </select>
+
+            <label for="category-filter">Categoría:</label>
+            <select id="category-filter" class="filter-dropdown">
+                <option value="">Todas las categorías</option>
+                <?php
+                $sql = "SELECT DISTINCT category_name FROM sneaker WHERE deleted_at IS NULL";
+                $result = mysqli_query($connection, $sql);
+
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $categoryName = $row['category_name'];
+                    echo "<option value='$categoryName'>$categoryName</option>";
+                }
+                ?>
+            </select>
+
+            <label for="search-input">Búsqueda:</label>
+            <input type="search" id="search-input" placeholder="Search sneaker">
+            <button id="search-button">Buscar</button>
         </div>
     </div>
 
     <div class="TablaContainerSneakers">
         <?php
         require_once "connection.php";
-        $sql ="SELECT * FROM sneaker
-        INNER JOIN stock ON sneaker.sneaker_id = stock.sneaker_id
-        WHERE sneaker.deleted_at IS NULL;";
+        $sql = "SELECT * FROM sneaker
+                INNER JOIN stock ON sneaker.sneaker_id = stock.sneaker_id
+                WHERE sneaker.deleted_at IS NULL;";
 
         $result = mysqli_query($connection, $sql);
 
@@ -39,14 +100,11 @@
                     <p>Precio: <?php echo $column['price']; ?></p>
                     <p>Stock: <?php echo $column['stock_quantity']; ?></p>
                 </div>
+                <!-- Acciones para el usuario normal -->
                 <div class="sneaker-actions">
-
-                
+                    <!-- Puedes agregar acciones específicas para el usuario normal aquí -->
                 </div>
             </div>
         <?php
         }
         ?>
-    </div>
-</body>
-</html>
