8ce0279d7c2ecf1f58212a405343c7bb5f12e49b
Axel <axesan917@gmail.com>
2023-11-20 12:07:39 -0600

    Reparación de los filtros en user

--- contents/sneakers-content_rol2.php
+++ contents/sneakers-content_rol2.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <script src="https://kit.fontawesome.com/c7fad14ccd.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
@@ -8,9 +7,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sneakers</title>
-</head>
 
+</head>
 <body>
+
 
     <div class="tab">
         <label class="tab-label" for="filterTab"><i class="fa-solid fa-chevron-down"></i></label>
@@ -34,6 +34,7 @@
             <select id="size-filter" class="filter-dropdown">
                 <option value="">Todas las tallas</option>
                 <?php
+                require_once "connection.php";
                 $sql = "SELECT DISTINCT size_number FROM size";
                 $result = mysqli_query($connection, $sql);
 
@@ -48,13 +49,14 @@
             <select id="model-filter" class="filter-dropdown">
                 <option value="">Todos los modelos</option>
                 <?php
+                require_once "connection.php";
                 $sql = "SELECT DISTINCT sneaker_name FROM sneaker WHERE deleted_at IS NULL";
                 $result = mysqli_query($connection, $sql);
 
                 while ($row = mysqli_fetch_assoc($result)) {
                     $sneakerName = $row['sneaker_name'];
                     echo "<option value='$sneakerName'>$sneakerName</option>";
-                }
+                }   
                 ?>
             </select>
 
@@ -62,6 +64,7 @@
             <select id="category-filter" class="filter-dropdown">
                 <option value="">Todas las categorías</option>
                 <?php
+                require_once "connection.php";
                 $sql = "SELECT DISTINCT category_name FROM sneaker WHERE deleted_at IS NULL";
                 $result = mysqli_query($connection, $sql);
 
@@ -78,46 +81,31 @@
         </div>
     </div>
 
-    <div class="TablaContainerSneakers">
-        <?php
-        $sql = "SELECT * FROM sneaker
-                INNER JOIN stock ON sneaker.sneaker_id = stock.sneaker_id
-                WHERE sneaker.deleted_at IS NULL;";
 
-        $result = mysqli_query($connection, $sql);
+    <div id="sneaker-container" class="TablaContainerSneakers">
+    </div>
 
-        while ($column = mysqli_fetch_array($result)) {
-        ?>
-            <div class="sneaker-card">
-                <div class="sneaker-image">
-                    <img src="<?php echo $column['imagen_url']; ?>" alt="Imagen del sneaker">
-                </div>
-                <div class="sneaker-info">
-                    <h2>Modelo: <?php echo $column['sneaker_name']; ?></h2>
-                    <p>Marca: <?php echo $column['brand_name']; ?></p>
-                    <p>Talla: <?php echo $column['size_number']; ?></p>
-                    <p>Precio: <?php echo $column['price']; ?></p>
-                    <p>Stock: <?php echo $column['stock_quantity']; ?></p>
-                </div>
-                <!-- Acciones para el usuario normal -->
-                <div class="sneaker-actions">
-                    <!-- Puedes agregar acciones específicas para el usuario normal aquí -->
-                </div>
-            </div>
-        <?php
-        }
-        ?>
 
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <div id="fetched-sneaker-container" class="fetched-sneaker-container">
+    </div>
+    
+    <div id="filtered-sneaker-container" class="filtered-sneaker-container">
+    </div>
 
-        <script src="scripts/showSneakers.js"></script>
-        <script src="scripts/search.js"></script>
-        <script src="scripts/filter.js"></script>
+    <div id="not-found-msg" class="not-found-msg"><h2> No se encontraron resultados </h2></div>
 
-        <?php include_once("modalEditSneaker.php"); ?>
+ 
 
 </body>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<script src="scripts/showSneakers-rol2.js"></script>
+<script src="scripts/search.js"></script>
+<script src="scripts/filter.js"></script>
+
+<?php include_once("modalEditSneaker.php"); ?>
 
 </html>
